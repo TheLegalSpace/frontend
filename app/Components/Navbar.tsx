b8e1d4ce8eb15e2136f9d509ba19c538c0e4df93
Oluwatobiloba Oluwadare <tobidechamp15@gmail.com>
2026-05-02 10:19:59 +0100

    sign in page and navigation from landing page

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,14 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Navbar () {
-    return (
-        <div className="flex justify-between items-center px-42 py-4 border-b">
-            <Image src="/logo.png" alt="Logo" width={112.36} height={32} />
+export default function Navbar() {
+  return (
+    <div className="flex justify-between items-center px-42 py-4 border-b">
+      <Image src="/logo.png" alt="Logo" width={112.36} height={32} />
 
-            <div className="flex gap-4 items-center">
-                <button className="text-sm">Lawyer Login</button>
-                <button className="text-sm bg-[#1A56DB] text-white px-3 py-2.5 rounded-lg gap-2.5">Find a Lawyer</button>
-            </div>
-        </div>
-    )
+      <div className="flex gap-4 items-center">
+        <Link href="/signin" className="text-sm">
+          Lawyer Login
+        </Link>
+
+        <button className="text-sm bg-[#1A56DB] text-white px-3 py-2.5 rounded-lg gap-2.5">
+          Find a Lawyer
+        </button>
+      </div>
+    </div>
+  );
 }
