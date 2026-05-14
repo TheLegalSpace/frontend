b151b0bbbd400fa03629a11e1af030e0ea5293f8
Dada Emmanuel <dadaemmanuel751@gmail.com>
2026-05-14 09:41:58 +0100

    added the notifications page

--- app/dashboard/notifications/page.tsx
+++ app/dashboard/notifications/page.tsx
@@ -1,7 +1,13 @@
-import React from "react";
-
-const page = () => {
-  return <div>Feedss</div>;
-};
-
-export default page;
+"use client";
+ 
+import { useAuth } from "@/app/context/AuthContext";
+import NotificationsPage from "@/app/Components/Notifications/NotificationsPage"; 
+ 
+export default function Page() {
+  const { user, isLoading } = useAuth();
+ 
+  if (isLoading) return null;
+  if (!user) return null;
+ 
+  return <NotificationsPage />;
+}
