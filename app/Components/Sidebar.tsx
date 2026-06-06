3107cd15c2c6713184b8e30aba862a26086b72da
Dada Emmanuel <dadaemmanuel751@gmail.com>
2026-06-07 00:29:53 +0100

    pushed legal news

--- app/Components/Sidebar.tsx
+++ app/Components/Sidebar.tsx
@@ -52,7 +52,7 @@ const NAV_ITEMS: Record<UserRole, NavItem[]> = {
     { label: "Leads", href: "/dashboard/leads", icon: Users },
     { label: "Messages", href: "/dashboard/messages", icon: MessageCircle },
     { label: "Posts", href: "/dashboard/posts", icon: MonitorUp },
-    { label: "Legal News", href: "/dashboard/legal-news", icon: BookOpenText },
+    { label: "Legal News", href: "/dashboard/Legal-News", icon: BookOpenText },
     {
       label: "TLS Research",
       href: "/dashboard/TLS-Research",
@@ -82,7 +82,7 @@ const NAV_ITEMS: Record<UserRole, NavItem[]> = {
       href: "/dashboard/TLS-Research",
       icon: FolderSearch,
     },
-    { label: "Legal News", href: "/dashboard/legal-news", icon: BookOpenText },
+    { label: "Legal News", href: "/dashboard/Legal-News", icon: BookOpenText },
     {
       label: "Legal Library",
       href: "/dashboard/legal-library",
@@ -107,7 +107,7 @@ const NAV_ITEMS: Record<UserRole, NavItem[]> = {
       href: "/dashboard/TLS-Research",
       icon: FolderSearch,
     },
-    { label: "Legal News", href: "/dashboard/legal-news", icon: BookOpenText },
+    { label: "Legal News", href: "/dashboard/Legal-News", icon: BookOpenText },
     {
       label: "Legal Library",
       href: "/dashboard/legal-library",
